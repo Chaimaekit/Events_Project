17fd5c0f2b4fb2ef46064a2e700a6b09d25709e3
chaimaekit <chaimaeeljerrar11@gmail.com>
2025-08-11 16:11:21 +0100

    run workflow v4

--- workflow.py
+++ workflow.py
@@ -4,6 +4,9 @@ from scrape.eventbrit import get_event_brit
 from scrape.eventsma import get_events_ma
 from scrape.guichet import get_guichet
 from into_db.connection import insert_events
+from prefect.deployments import DeploymentSpec
+from prefect.filesystems import GitHub
+from prefect.server.schemas.schedules import CronSchedule
 
 @task
 def insert_casa_events():
@@ -37,6 +40,19 @@ def print_events():
     third_event = insert_events_ma()
     forth_event = insert_guichet()
     print(third_event)
+
+
+DeploymentSpec(
+    flow=print_events,
+    name="scraper",
+    work_pool_name="default",
+    schedule=CronSchedule(cron="*/2 * * * *"),
+    storage=GitHub(
+        repo="Chaimaekit/Events_Project",
+        path="workflow.py",
+        ref="main"
+    ),
+).apply()
 
 if __name__ == "__main__":
     print_events.deploy(
