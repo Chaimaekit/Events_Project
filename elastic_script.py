e3a630058024111ba8e4c8f7de15522ca035d2d3
chaimaekit <chaimaeeljerrar11@gmail.com>
2025-08-13 12:16:20 +0100

    prefect elstic

--- elastic_script.py
+++ elastic_script.py
@@ -1,15 +1,28 @@
 from elasticsearch import Elasticsearch
 from into_db.connection import get_db_events
+from scrape.casaevents import get_casa_events
+from scrape.eventbrit import get_event_brit
+from scrape.eventsma import get_events_ma
+from scrape.guichet import get_guichet
 
-es = Elasticsearch("http://localhost:9200", verify_certs=False)
+es = Elasticsearch("http://localhost:9200", basic_auth=("elastic", "8jfe*MB6Z-9r8PsuVzDs"))
 
 def indexing():
+    events = []
     if not es.ping():
         raise ValueError("Connection failed. Make sure Elasticsearch is running.")
 
     print("Connected to Elasticsearch.")
 
-    events = get_db_events()
+
+    for result in get_casa_events():
+        events.append(result)
+    for result in get_event_brit():
+        events.append(result)
+    for result in get_events_ma():
+        events.append(result)
+    for result in get_guichet():
+        events.append(result)
 
     for event in events:
         event.pop("_id")
@@ -27,3 +40,5 @@ def check_doc(index_name, event):
     })
     if result:
         return result["hits"]["hits"]
+    
+indexing()
